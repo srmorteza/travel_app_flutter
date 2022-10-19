6a39716254016a3b3cb3c1bf7f7ffebae67a4ed6
sr.morteza <sr.morteza@gmail.com>
2022-10-19 13:09:37 +0330

    complate the first page(slider) with text component and button component 1401-7-27

--- lib/pages/wellcome_page.dart
+++ lib/pages/wellcome_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:travel/misc/colors.dart';
+import 'package:travel/widgets/app_large_text.dart';
+import 'package:travel/widgets/app_text.dart';
+import 'package:travel/widgets/responsive_button.dart';
 
 class WellcomPage extends StatefulWidget {
   const WellcomPage({Key? key}) : super(key: key);
@@ -28,7 +32,58 @@ class _WellcomPageState extends State<WellcomPage> {
               height: double.maxFinite,
               decoration: BoxDecoration(
                   image: DecorationImage(
+                    fit: BoxFit.cover,
                       image: AssetImage("img/"+images[index]))),
+              child: Container(
+                margin: const EdgeInsets.only(top: 150,left: 20,right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                       AppLargeText(text: "Trips"),
+                       AppText(text: "Mountain" , size: 30,),
+                        SizedBox(height: 20,),
+                        Container(
+                          width: 250,
+                          child: AppText(
+                            text: "Moraine Lake is yet another astonishing ",
+                            color: AppColors.textColor2,
+                            size: 14,
+                          ),
+                        ),
+                        SizedBox(height: 40,),
+                        ResponsiveButton()
+
+
+
+
+
+
+
+
+
+
+                      ],
+                    ),
+                    Column(
+                      children: List.generate(3, (indexDot) {
+                        return Container(
+                          margin: const EdgeInsets.only(bottom: 3),
+                          width: 8,
+                          height: index==indexDot?25:8,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: index==indexDot? AppColors.mainColor : AppColors.mainColor.withOpacity(0.3),
+
+                          ),
+                        ) ;
+                      }),
+                    )
+                  ],
+                ),
+              ),
             );
           }),
     );
